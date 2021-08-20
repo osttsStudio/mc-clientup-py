b77a6e877d709bda3839f2f7ba0489622b53b05d
狐日泽 <Sun_Set1102@outlook.com>
2021-08-21 01:49:29 +0800

    v0.1.9

--- mc_clientup_example.py
+++ mc_clientup_example.py
@@ -1,14 +1,14 @@
 # ====================================================
 
 # Author:狐日泽
-# Version:0.1.7
+# Version:0.1.9
 # name:mc_clientup_py
 
-# 基于python的简单粗暴mc客户端自动更新程序
+# 基于python3的简单粗暴mc客户端自动更新程序
 # 程序会下载服务器配置文件并和本地配置文件进行比对
 # 如果版本号大于本地文件，会自动下载更新包并解压覆盖，然后删除服务器配置文件
 
-# Simple and rude mc client automatic update program based on python.
+# Simple and rude mc client automatic update program based on python3.
 # The program will download the server configuration file and compare it with the local configuration file.
 # If the version number is greater than the local file, the update package will be automatically downloaded and decompressed and overwritten, and then the server configuration file will be deleted.
 
@@ -28,9 +28,12 @@ os.system("") # fixd print's color bug in Win10
 Con_res = os.getcwd() + "./config_new.ini" # config_new.ini--server config file name
 Zip_res = os.getcwd() + "./xxx.zip" # update file name
 bat_res = os.getcwd() + "/.minecraft/update.bat" # a bat file for updating itself and config, and supports deleting unnecessary files
+Ver_res = os.getcwd() + "./version.txt" # update logs name
+Ver_url = "http://xxx/xxx.txt" # update logs
 Con_url = "http://xxx/xxx.ini" # config file url
 
-request.urlretrieve(Con_url,Con_res)
+request.urlretrieve(Con_url,Con_res)  # download server config file
+request.urlretrieve(Ver_url,Ver_res)  # download update logs
 
 # load local config
 con = ConfigParser()
@@ -64,7 +67,9 @@ if Ver_local == Ver_server:
 
 if Ver_local < Ver_server:
 
-    print("Downloading...Please wait.")
+    with open(r"version.txt", encoding="utf-8") as file:
+        print(file.read())
+        print("\033[5;36;40mDownloading...Please wait.\033[0m")
 
     def get_data():
         Zip_url = "http://xxx/xxx.zip" # update file url
@@ -75,7 +80,6 @@ if Ver_local < Ver_server:
     Zip_url, data = get_data()
 
     Temp_file = tempfile.TemporaryFile()
-    # print(Temp_file)
 
     Temp_file.write(data)
 
@@ -87,6 +91,7 @@ if Ver_local < Ver_server:
     Temp_file.close()
     
 time.sleep(2)
+os.remove('version.txt')
 os.remove('config_new.ini')
 os.system('client.exe')
 os.system('resourcepacks.exe')
