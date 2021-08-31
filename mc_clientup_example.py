d00d1d5507ac49b316669bd062e4d8cbbb9b4fa5
狐日泽 <Sun_Set1102@outlook.com>
2021-09-01 01:15:32 +0800

    v0.2.4
    add：error log
    add：更新日志

--- mc_clientup_example.py
+++ mc_clientup_example.py
@@ -1,14 +1,14 @@
 # ====================================================
 
 # Author:狐日泽
-# Version:0.2.3
+# Version:0.2.4
 # name:mc_clientup_py
 
-# 基于python3的简单粗暴mc客户端自动更新程序
+# 基于python的简单粗暴mc客户端自动更新程序
 # 程序会下载服务器配置文件并和本地配置文件进行比对
 # 如果版本号大于本地文件，会自动下载更新包并解压覆盖，然后删除服务器配置文件
 
-# Simple and rude mc client automatic update program based on python3.
+# Simple and rude mc client automatic update program based on python.
 # The program will download the server configuration file and compare it with the local configuration file.
 # If the version number is greater than the local file, the update package will be automatically downloaded and decompressed and overwritten, and then the server configuration file will be deleted.
 
@@ -18,28 +18,49 @@ import os
 import sys
 import time
 import zipfile
+import logging
 import requests
 import tempfile
+import traceback
 from urllib import request
 from configparser import ConfigParser
 
+os.remove('error.log') # del old log file
+
+logging.basicConfig(filename='error.log',level=logging.DEBUG,format="%(asctime)s - %(pathname)s - %(message)s",datefmt="%Y/%m/%d %H:%M:%S")
+
 os.system("") # fixd print's color bug in Win10
 
-Con_res = os.getcwd() + "./config_new.ini" # config_new.ini--server config file name
-bat_res = os.getcwd() + "/.minecraft/update.bat" # a bat file for updating itself and config, and supports deleting unnecessary files
-Ver_res = os.getcwd() + "./version.txt" # update logs name
-Client_res = os.getcwd() + "./client.ini" # launcher name file
-Ver_url = "http://xxx/xxx.txt" # update logs
-Con_url = "http://xxx/xxx.ini" # config file url
-Client_url = "http://chii.tpmc.top/client.ini" # launcher name file url
+Con_res = os.getcwd() + "./config_new.ini"
+bat_res = os.getcwd() + "/.minecraft/update.bat"
+Ver_res = os.getcwd() + "./version.txt"
+Client_res = os.getcwd() + "./client.ini"
+Ver_url = "http://xxx/xxx.txt"
+Con_url = "http://xxx/config.ini"
+Client_url = "http://xxx/client.ini"
 
-request.urlretrieve(Con_url,Con_res)  # download server config file
-request.urlretrieve(Ver_url,Ver_res)  # download update logs
+try:
+    request.urlretrieve(Con_url,Con_res)  # download server config file
+    request.urlretrieve(Ver_url,Ver_res)  # download update logs
+
+except:
+    logging.debug(traceback.format_exc())
 
 # load local config
 con = ConfigParser()
 con_path = os.path.join(os.path.abspath('.'),'config.ini') # config.ini--local config file name
 con.read(con_path)
+
+try:
+    if not os.path.exists(con_path):
+        print("\033[1;31;40mFile config.ini does not exist,please re-download.url:none\033[0m")
+        input("Press Enter to exit.")
+        os.remove('config_new.ini')
+        os.remove('version.txt')
+        sys.exit('config not found')
+except:
+    logging.debug(traceback.format_exc())
+
 Ver_local = con.get('ver','version')
 Mc_local = con.get('ver','mc_version')
 
@@ -50,15 +71,18 @@ con_server.read(con_serverpath)
 Ver_server = con_server.get('ver','version')
 Mc_server = con_server.get('ver','mc_version')
 
-if not os.path.exists(Client_res):
-    request.urlretrieve(Client_url,Client_res)  # download launcher name file
-    print("""
+try:
+    if not os.path.exists(Client_res):
+        request.urlretrieve(Client_url,Client_res)  # download launcher name file
+        print("""
 client.ini下载成功，如启动器名称不是默认的client.exe请修改client.ini文件后重新运行。
 The client.ini download is successful, if the launcher name is not the default：client.exe, please modify the client.ini file and run again.""")
-    input("Press Enter to exit.")
-    os.remove('version.txt')
-    os.remove('config_new.ini')
-    sys.exit(0)
+        input("Press Enter to exit.")
+        os.remove('version.txt')
+        os.remove('config_new.ini')
+        sys.exit()
+except:
+    logging.debug(traceback.format_exc())
 
 # launcher name config
 Client_ini = ConfigParser()
@@ -67,55 +91,60 @@ Client_ini.read(Client_path)
 Client = Client_ini.get('setting','name')
 
 # config.ini does not exist
-if not os.path.exists(con_path):
-    print("\033[1;31;40mFile config.ini does not exist,please re-download.url:none\033[0m")
-    input("Press Enter to exit.")
-    os.remove('config_new.ini')
-    os.remove('version.txt')
-    sys.exit(1)
 
-if int(Ver_local) == int(Ver_server):
-    os.system(Client)
-    os.remove('version.txt')
-    os.remove('config_new.ini')
-    sys.exit(0)
+try:
+    if int(Ver_local) == int(Ver_server):
+        os.system(Client)
+        os.remove('version.txt')
+        os.remove('config_new.ini')
+        sys.exit()
 
-if int(Ver_local) < int(Ver_server):
+except:
+    logging.debug(traceback.format_exc())
 
-    with open(r"version.txt", encoding="utf-8") as file:
-        print(file.read())
-        print('更新程序版本：0.2.3')
-        print(f"目前MC版本：{Mc_local}  最新MC版本：{Mc_server}")
-        print("\033[5;36;40mDownloading...Please wait.\033[0m")
+try:
+    if int(Ver_local) < int(Ver_server):
 
-    def get_data():
-        Zip_url = "http://xxx/xxx.zip"
-        response = requests.get(Zip_url)
-        return Zip_url, response.content
+        with open(r"version.txt", encoding="utf-8") as file:
+            print(file.read())
+            print('更新程序版本：0.2.4')
+            print(f"目前MC版本：{Mc_local}  最新MC版本：{Mc_server}")
+            print("\033[5;36;40mDownloading...Please wait.\033[0m")
 
-    Zip_url, data = get_data()
+        def get_data():
+            Zip_url = "http://xxx/xxx.zip"
+            response = requests.get(Zip_url)
+            return Zip_url, response.content
 
-    Temp_file = tempfile.TemporaryFile()
+        Zip_url, data = get_data()
 
-    Temp_file.write(data)
+        Temp_file = tempfile.TemporaryFile()
 
-    Unzip = zipfile.ZipFile(Temp_file, mode='r')
-    for names in Unzip.namelist():
-        Unzip.extract(names, './.minecraft')  # unzip to .minecraft
+        Temp_file.write(data)
 
-    Unzip.close()
-    Temp_file.close()
+        Unzip = zipfile.ZipFile(Temp_file, mode='r')
+        for names in Unzip.namelist():
+            Unzip.extract(names, './.minecraft')  # unzip to .minecraft
+
+        Unzip.close()
+        Temp_file.close()
     
-    time.sleep(2)
-    os.remove('version.txt')
-    os.system('resourcepacks.exe')
-    os.system(Client)
-    os.remove('config_new.ini')
-    sys.exit(0)
+        time.sleep(2)
+        os.remove('version.txt')
+        os.system('resourcepacks.exe')
+        os.system(Client)
+        os.remove('config_new.ini')
+        sys.exit()
 
-if int(Ver_local) > int(Ver_server):
-    print("\033[1;31;40mThe version is wrong,please re-dowoload. url:none\033[0m")
-    input("Press Enter to exit.")
-    os.remove('config_new.ini')
-    os.remove('version.txt')
-    sys.exit(1)
+except:
+    logging.debug(traceback.format_exc())
+
+try:
+    if int(Ver_local) > int(Ver_server):
+        print("\033[1;31;40mThe version is wrong,please re-dowoload. url:none\033[0m")
+        input("Press Enter to exit.")
+        os.remove('config_new.ini')
+        os.remove('version.txt')
+        sys.exit('version error')
+except:
+    logging.debug(traceback.format_exc())
--- update.bat
+++ update.bat
@@ -1,6 +1,7 @@
 mkdir bg
 move .minecraft\background.png .\bg\
 move .minecraft\config.ini .\
-move .minecraft\start.py .\
 move .minecraft\start.exe .\
 move .minecraft\resourcepacks.exe .\
+move .minecraft\2[basic]I18nupdatemod-1.0.8.jar .\optional-mods
+del /f .minecraft\mods\4[prolate]Traveler's Backpack-1.0.34.jar
