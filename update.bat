51dae1e4a25484fa27c010dbee1042d3623974f1
狐日泽 <Sun_Set1102@outlook.com>
2021-09-15 17:42:21 +0800

    fix a bug of
    Fixed an issue where files with spaces in the file name could not be deleted due to incorrect code

--- update.bat
+++ update.bat
@@ -4,4 +4,5 @@ move .minecraft\config.ini .\
 move .minecraft\start.exe .\
 move .minecraft\resourcepacks.exe .\
 move .minecraft\2[basic]I18nupdatemod-1.0.8.jar .\optional-mods
-del /f .minecraft\mods\4[prolate]Traveler's Backpack-1.0.34.jar
+del /f ".minecraft\mods\4[prolate]Traveler's Backpack-1.0.34.jar"
+del /f ".minecraft\resourcepacks\uzuki chii-san.zip"
